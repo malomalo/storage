a08893035fca660f0653745945e468d2a5d8cb0e
Jon Bracy <jonbracy@gmail.com>
2021-07-13 22:33:36 -0500

    Lazy load the connection to B2

--- lib/standard_storage/b2.rb
+++ lib/standard_storage/b2.rb
@@ -10,8 +10,6 @@ class StandardStorage::B2 < StandardStorage
     @bucket = configs[:bucket]
     @prefix = configs[:prefix]
     @key_id = configs[:key_id]
-
-    @client = ::B2.new(key_id: @key_id, secret: configs[:secret]).bucket(@bucket)
   end
   
   def local?
@@ -19,7 +17,7 @@ class StandardStorage::B2 < StandardStorage
   end
 
   def url(key, **options)
-    @client.get_download_url(destination(key), **options)
+    client.get_download_url(destination(key), **options)
   end
 
   def destination(key)
@@ -27,14 +25,14 @@ class StandardStorage::B2 < StandardStorage
   end
 
   def exists?(key)
-    @client.has_key?(destination(key))
+    client.has_key?(destination(key))
   end
 
   def write(key, file, meta_info={})
     file = file.tempfile if file.class.name == "ActionDispatch::Http::UploadedFile"
     file = File.open(file) if file.is_a?(String)
     
-    @client.upload_file(destination(key), file,
+    client.upload_file(destination(key), file,
       mime_type: meta_info[:content_type],
       sha1: meta_info[:sha1],
       content_disposition: meta_info[:filename] ? "inline; filename=\"#{::B2.encode(meta_info[:filename])}\"" : nil
@@ -42,37 +40,43 @@ class StandardStorage::B2 < StandardStorage
   end
   
   def cp(key, to)
-    @client.download(destination(key), to)
+    client.download(destination(key), to)
   rescue ::B2::NotFound => e
     raise Errno::ENOENT.new(e.message)
   end
 
   def read(key, &block)
-    @client.download(destination(key), &block)
+    client.download(destination(key), &block)
   rescue ::B2::NotFound => e
     raise Errno::ENOENT.new(e.message)
   end
 
   def delete(key)
-    @client.delete!(destination(key))
+    client.delete!(destination(key))
   end
   
   def sha1(key)
-    @client.file(destination(key)).sha1
+    client.file(destination(key)).sha1
   rescue ::B2::NotFound => e
     raise Errno::ENOENT.new(e.message)
   end
 
   def last_modified(key)
-    @client.file(destination(key)).uploaded_at
+    client.file(destination(key)).uploaded_at
   rescue ::B2::NotFound => e
     raise Errno::ENOENT.new(e.message)
   end
   
   def mime_type(key)
-    @client.file(destination(key)).mime_type
+    client.file(destination(key)).mime_type
   rescue ::B2::NotFound => e
     raise Errno::ENOENT.new(e.message)
+  end
+  
+  private
+  
+  def client
+    @client ||= ::B2.new(key_id: @key_id, secret: configs[:secret]).bucket(@bucket)
   end
   
 end
